40637bf0d6486e5136a57b665d196fd4354c0d67
ProjectADTP <38851250+ProjectADTP@users.noreply.github.com>
2025-03-25 18:37:31 +0300

    Update

--- Assets/scripts/ClosedArea.cs
+++ Assets/scripts/ClosedArea.cs
@@ -1,64 +1,25 @@
 using System;
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class ClosedArea : MonoBehaviour
 {
-    [SerializeField] private AudioClip _clip;
-    [SerializeField] private AudioSource _audioSource;
-
-    private float _delay;
-    private float _SoundChangeValue = 0.01f;
-    private float _minVolumeValue = 0f;
-    private float _maxVolumeValue = 1f;
-
-
-    private void Start()
-    {
-        _audioSource.clip = _clip;
-
-        _delay = 1 / 10;
-    }
+    public event Action<ClosedArea> Entered;
+    public event Action<ClosedArea> Exited;
 
     private void OnTriggerEnter2D(Collider2D other)
     {
-        if (other.GetComponent<ThiefMovement>() == false)
+        if (other.TryGetComponent<ThiefMovement>(out ThiefMovement component) == false)
             return;
 
-        _audioSource.volume = _minVolumeValue;
-        _audioSource.Play();
-
-        StartCoroutine(VolumeIncrease());
+        Entered?.Invoke(this);
     }
 
     private void OnTriggerExit2D(Collider2D other)
     {
-        if (other.GetComponent<ThiefMovement>() == false)
+        if (other.TryGetComponent<ThiefMovement>(out ThiefMovement component) == false)
             return;
 
-        StartCoroutine(VolumeDecrease());
-    }
-
-    private IEnumerator VolumeIncrease()
-    {
-        while (_audioSource.volume < _maxVolumeValue)
-        {
-            yield return new WaitForSeconds(_delay);
-
-            _audioSource.volume += _SoundChangeValue;
-        }
-    }
-
-    private IEnumerator VolumeDecrease()
-    {
-        while (_audioSource.volume > _minVolumeValue)
-        { 
-            yield return new WaitForSeconds(_delay);
-
-            _audioSource.volume -= _SoundChangeValue;
-        }
-
-        _audioSource.Stop();
+        Exited?.Invoke(this);
     }
 }
---
+++ Assets/scripts/SoundChanger.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public class SoundChanger : MonoBehaviour
+{
+    [SerializeField] private AudioClip _clip;
+    [SerializeField] private AudioSource _audioSource;
+    [SerializeField] private ClosedArea _closedArea;
+
+    private float _SoundChangeValue = 0.1f;
+    private float _minVolumeValue = 0f;
+    private float _maxVolumeValue = 1f;
+
+    private WaitForSeconds _delay;
+
+    private float _second = 1f;
+    private float _timeDivider = 10f;
+
+    private void Start()
+    {
+        _delay = new WaitForSeconds(_second / _timeDivider);
+
+        _audioSource.clip = _clip;
+
+        _closedArea.Entered += Increase;
+        _closedArea.Exited += Decrease;
+    }
+
+    private void Increase(ClosedArea area)
+    {
+        _audioSource.Play();
+        _audioSource.volume = _minVolumeValue;
+
+        StartCoroutine(ChangeVolume(_maxVolumeValue));
+    }
+
+    private void Decrease(ClosedArea area)
+    {
+        StartCoroutine(ChangeVolume(_minVolumeValue));
+    }
+
+    public IEnumerator ChangeVolume(float goal)
+    {
+        while (_audioSource.volume != goal)
+        {
+            yield return _delay;
+
+            if (goal == _maxVolumeValue)
+                _audioSource.volume += _SoundChangeValue;
+            else
+                _audioSource.volume -= _SoundChangeValue;
+        }
+
+        if ((_audioSource.volume == _minVolumeValue) && (goal == _minVolumeValue))
+            _audioSource.Stop();
+    }
+}
